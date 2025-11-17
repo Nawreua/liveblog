4cc2e4ff2693161498802f0676f83bab6a8daffa
Nawreua <nawreua@gmail.com>
2025-11-17 23:28:47 +0100

    app: add debug mode and basic post view

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,33 @@
+from argparse import ArgumentParser
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-from app.configuration import DebugConfig
-app.config.from_object(DebugConfig)
+parser = ArgumentParser(prog='liveblog',
+                        description='A small sized blog')
+parser.add_argument('--debug', help='Launch the application in Debug Mode', action='store_true')
+args = parser.parse_args()
+
+if args.debug:
+    from app.configuration import DebugConfig
+    app.config.from_object(DebugConfig)
+else:
+    from app.configuration import Config
+    app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+
+if args.debug:
+    from app.models import Post
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+
+        db.session.add(Post(author='Erwan', title='Test post', content='Un exemple de post'))
+        db.session.commit()
+
+        print(Post.query.all())
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,5 +1,28 @@
+from flask import render_template
+
 from app import app
+from app.models import Post
 
 @app.route("/")
 def hello_world():
+    """
+    Display the main page
+    """
     return "<p>Hello, World!</p>"
+
+@app.route("/view/")
+def view_all_posts():
+    """
+    Display all posts
+    """
+    return "<p>FIXME</p>"
+
+@app.route("/view/<id>")
+def view_post(id):
+    """
+    Display a given post
+
+    Arguments:
+    - id: the id of the post 
+    """
+    return render_template('view.html', post=Post.query.get(id))
