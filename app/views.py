89bf5a65b6d21884948ca1ffed08abd4923d2c29
Nawreua <nawreua@gmail.com>
2026-01-25 15:03:40 +0100

    views: add log and json view answer

--- app/views.py
+++ app/views.py
@@ -30,10 +30,16 @@ def view_all_posts():
     - limit: number of posts to load, by default 25
     - offset: id of the first post to load from
     """
+    app.logger.info('/view/ route requested')
     offset = request.args.get('offset', 0)
+    app.logger.debug(f'With offset {offset}')
     limit = request.args.get('limit', 25)
+    app.logger.debug(f'With limit {limit}')
+    app.logger.info('Fetch posts')
+    posts = Post.get_multiple_posts(offset, limit).all()
+    app.logger.debug(posts)
     if _no_accept_html(request):
-        return jsonify(Post.get_multiple_posts(offset, limit))
+        return jsonify(posts)
     return "<p>FIXME</p>"
 
 @app.route("/view/<id>")
@@ -44,7 +50,11 @@ def view_post(id):
     Route arguments:
     - id: the id of the post 
     """
+    app.logger.info('/view/<id> requested')
+    app.logger.debug(f'With id {id}')
+    app.logger.info('Fetch post')
     post = Post.get_post(id)
+    app.logger.debug(post)
     if _no_accept_html(request):
         return jsonify(post)
     return render_template('view.html', post=post)
@@ -66,15 +76,20 @@ def save_post_form():
     - Post data, as a JSON
     - Authentification data
     """
+    app.logger.info('/save/ requested')
     post_data = request.json
+    app.logger.debug(f'With data {post_data}')
     try:
         new_post = Post(
             author=post_data.get('author'),
             title=post_data.get('title'),
             content=post_data.get('content'),
             date=datetime.now())
+        app.logger.info('Add new post')
+        app.logger.debug(new_post)
         db.session.add(new_post)
         db.session.commit()
     except exc.IntegrityError:
+        app.logger.error('New post creation failed')
         abort(422)
     return render_template('view.html', post=Post.get_post(new_post.id))
