ce863d41ba30d0f533137f437c748cbaecb536fe
Nawreua <nawreua@gmail.com>
2026-01-26 14:13:35 +0100

    client: improve logging and add latest_posts entrypoint

--- client/client.py
+++ client/client.py
@@ -1,12 +1,14 @@
 import cmd
 import re
 import logging
+import sys
 from argparse import ArgumentParser
 
 import requests
 from requests.auth import HTTPBasicAuth
 
-logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stderr)
+logger = logging.getLogger("Liveblog Client")
 
 class LiveblogClient(cmd.Cmd):
     intro = 'Welcome to the liveblog client. Type help or ? to list commands.\n'
@@ -20,22 +22,28 @@ class LiveblogClient(cmd.Cmd):
     def __init__(self, completekey = "tab", stdin = None, stdout = None, base_url='', keypass='', user=''):
         super().__init__(completekey, stdin, stdout)
         if base_url != '':
-            self.do_setup_url(base_url)
+            self.setup_url(base_url)
         self.keypass = keypass
         self.user = user
 
-    def do_setup_url(self, url):
-        'Configure the URL used by the client: setup_url http://127.0.0.1:5000/'
-
+    def setup_url(self, url):
         if self._url_pattern is None:
             # Using [Liberal Regex Pattern for Web URLs by @gruber](https://gist.github.com/gruber/8891611)
             # License: https://opensource.org/license/bsd-3-clause
             self._url_pattern = re.compile("""(?i)\\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\\s()<>{}\\[\\]]+|\\([^\\s()]*?\\([^\\s()]+\\)[^\\s()]*?\\)|\\([^\\s]+?\\))+(?:\\([^\\s()]*?\\([^\\s()]+\\)[^\\s()]*?\\)|\\([^\\s]+?\\)|[^\\s`!()\\[\\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\\b/?(?!@)))""")
+
         if self._url_pattern.fullmatch(url) is None:
-            print(f'Erroneous base URL configured: {url}')
+            return False
         else:
             self.base_url = url if url.endswith('/') else url + '/'
             logger.info(f'New URL is {self.base_url}')
+            return True
+
+    def do_setup_url(self, url):
+        'Configure the URL used by the client: setup_url http://127.0.0.1:5000/'
+
+        if not self.setup_url(url):
+            print(f'Erroneous base URL configured: {url}')
 
     def do_setup_user(self, user):
         'Configure the user used by the client: setup_user Test'
@@ -56,6 +64,14 @@ class LiveblogClient(cmd.Cmd):
             post = response.json()
             logger.info(post)
             pretty_print_post(post)
+
+    def do_fetch_latest_posts(self, _):
+        'Request and output the latest blog posts: fetch_latest_posts'
+        if (response := self.request_get('')) is not None:
+            posts = response.json()
+            logger.info(posts)
+            for post in reversed(posts):
+                pretty_print_post(post)
 
     def do_fetch_posts(self, arg):
         'Request and output multiple blog posts: fetch_posts 1 25'
@@ -112,6 +128,7 @@ class LiveblogClient(cmd.Cmd):
         return True
 
     def check_response(self, response: requests.Response):
+        logger.info(f'Responded with ${response.status_code}')
         if not response.ok:
             print(f'Response error with code: {response.status_code}')
             return False
@@ -121,8 +138,15 @@ class LiveblogClient(cmd.Cmd):
         complete_url = self.base_url + url
         if not self.check_request(complete_url):
             return None
+
         logger.info(f'Requesting {complete_url} with POST')
-        response = requests.post(complete_url, auth=HTTPBasicAuth(self.user, self.keypass), params=payload, json=json)
+        try:
+            response = requests.post(complete_url, auth=HTTPBasicAuth(self.user, self.keypass), params=payload, json=json)
+        except requests.ConnectionError as error:
+            logger.info(f'Failure to connect ({error})')
+            print('Request failed, check URL or server status')
+            return None
+
         if self.check_response(response):
             return response
         return None
@@ -131,8 +155,15 @@ class LiveblogClient(cmd.Cmd):
         complete_url = self.base_url + url
         if not self.check_request(complete_url):
             return None
+
         logger.info(f'Requesting {complete_url} with GET')
-        response = requests.get(complete_url, auth=HTTPBasicAuth(self.user, self.keypass), params=payload)
+        try:
+            response = requests.get(complete_url, auth=HTTPBasicAuth(self.user, self.keypass), params=payload)
+        except requests.ConnectionError as error:
+            logger.info(f'Failure to connect ({error})')
+            print('Request failed, check URL or server status')
+            return None
+
         if self.check_response(response):
             return response
         return None
